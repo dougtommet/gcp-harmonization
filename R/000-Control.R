6912b576060e87f7d03560f85e12d2cdf6c7bb88
dougtommet <doug_tommet@brown.edu>
2023-03-27 15:46:56 -0400

    Updated DIF analysis report

--- R/000-Control.R
+++ R/000-Control.R
@@ -12,4 +12,8 @@ fs::file_move(here::here("R", "xxx-Comparison_of_GCP_versions.pptx"),
 fs::file_move(here::here("R", "xxx-Tables_and_figures_for_manuscript.html"), 
               here::here("Reports", stringr::str_c("Tables_and_figures_for_manuscript_", Sys.Date(),".html")))
 
+fs::file_move(here::here("R", "xxx-DIF_analysis.html"), 
+              here::here("Reports", stringr::str_c("DIF_analysis_", Sys.Date(),".html")))
 
+fs::file_move(here::here("R", "xxx-Duke-GCP-tables-and-figures.html"), 
+              here::here("Reports", stringr::str_c("Duke_GCP_tables_and_figures_", Sys.Date(),".html")))
